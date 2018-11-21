0006b0fe80d9c8d6f6c4908813ba1f69640adfc8
Malte <msp3@online.de>
2018-11-21 23:06:10 +0100

    code works!

--- solvethesecret.py
+++ solvethesecret.py
@@ -1,13 +1,19 @@
 import os
 
 def renameFiles():
-    allFiles = os.listdir(r"E:\gitrepos\learnpython\prank")
+    workingDir = r"E:\gitrepos\learnpython\prank"
+    
+    allFiles = os.listdir(workingDir)
     print(allFiles)
+
+    os.chdir(workingDir)
+    print("Switched to new working dir: " + workingDir)
 
     for file in allFiles:
         noDigits = str.maketrans("", "", "0123456789")
         os.rename(file, file.translate(noDigits))
 
+    allFiles = os.listdir(workingDir)
     print(allFiles)
 
 renameFiles()
